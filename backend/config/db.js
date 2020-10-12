c4901142d93f557d2f039fb24b678074d17dd99a
canti <ljdemar91@gmail.com>
2020-10-11 19:46:38 -0500

    added colors package but it didnt work so didnt use it

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
 //database configuration
 import mongoose from 'mongoose';
 
+
+
 const connectDB = async () => {
     try {
         const connection = await mongoose.connect(process.env.MONGO_URI, {
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
+
+
 dotenv.config();
 connectDB();
 
