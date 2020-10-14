8f93cc89f5ea5a085d4fab789b8b3ad4602dd8a1
canti <ljdemar91@gmail.com>
2020-10-14 15:59:19 -0500

    set up types, made product reducer and action, connectedd to store

---
+++ frontend/src/actions/products.js
@@ -0,0 +1,29 @@
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_REQUEST_SUCCESS,
+  PRODUCT_LIST_REQUEST_FAILURE,
+} from "./types";
+import axios from "axios";
+
+export const listProducts = () => async (dispatch) => {
+  try {
+    //dispatch request
+    dispatch({
+      type: PRODUCT_LIST_REQUEST,
+    });
+    const { data } = await axios.get("/api/products");
+    //dispatch success
+    dispatch({
+      type: PRODUCT_LIST_REQUEST,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_REQUEST_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.data.message
+          : error.message,
+    });
+  }
+};
