091df8f37154a7953a706938fb0b7c615f5f840c
canti <ljdemar91@gmail.com>
2020-10-21 15:45:09 -0500

    created login action and reducer and added userinfo to initial state if present

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,38 @@
+import { USER_LOGIN_REQUEST, USER_LOGIN_REQUEST_SUCCESS, USER_LOGIN_REQUEST_FAIL } from "./types";
+import axios from 'axios';
+
+//action that requests login token
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+
+        //check if header has correct content type for token read 
+        const configuration = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        //make request
+        const { data } = await axios.post('/api/users/login', { email, password }, configuration)
+
+        //dispatch success
+        dispatch({
+            type: USER_LOGIN_REQUEST_SUCCESS,
+            payload: data
+        })
+
+        //set user to local storage
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_REQUEST_FAIL,
+            payload:
+              error.response && error.response.data.message
+                ? error.data.message
+                : error.message,
+          });
+    }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,22 +1,37 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import { productListReducer, productInfoReducer } from './reducers/productReducers.js';
-import { cartReducer } from './reducers/cartReducer';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import {
+  productListReducer,
+  productInfoReducer,
+} from "./reducers/productReducers.js";
+import { cartReducer } from "./reducers/cartReducer";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
-    productList: productListReducer,
-    productInfo: productInfoReducer,
-    cart: cartReducer,
+  productList: productListReducer,
+  productInfo: productInfoReducer,
+  cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
-const cartItemsFromLocal = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+//get data from local
+const cartItemsFromLocal = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+const userInfoFromLocal = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
-const initialState= {
-    cart: { cartItems: cartItemsFromLocal }
-}
-const middleware = [thunk]
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
-
+const initialState = {
+  cart: { cartItems: cartItemsFromLocal },
+  userLogin: { userInfo: userInfoFromLocal }
+};
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
